962adae54afacf0cdcd3fb523c65c8eda716199d
Can <canzhang2019@gmail.com>
2022-05-31 11:22:58 +0800

    yaw_down

--- 6.yaw_environment/Zhoushan_mesh_two_effected_areas/python.sh
+++ 6.yaw_environment/Zhoushan_mesh_two_effected_areas/python.sh
@@ -12,7 +12,7 @@ do
 # done
 for i in 1.0
 do
-cp ../../../outputs/6.yaw_environment/Paper3/Zhoushan_mesh/optimisation/backhome-two_effected/intermediate-op-l_y-P_factor_$i-5min_e\&v/diagnostic_controls.hdf5 ./
+cp ../../../outputs/6.yaw_environment/Paper3/Zhoushan_mesh/optimisation/backhome-two_effected/intermediate-op-l_y-P_factor_$i-5min_e\&v3/diagnostic_controls.hdf5 ./
 cp ly-forward.py $i.py
 mpirun -np 8 python $i.py;
 rm $i.py
